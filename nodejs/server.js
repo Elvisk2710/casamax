4750f4526c457d40f9aaae381ab77311ee3fd933
elvisk2710 <kadeyaelvis@gmail.com>
2024-12-27 18:46:53 +0200

    fixing token

--- nodejs/server.js
+++ nodejs/server.js
@@ -242,20 +242,23 @@ app.get("/webhook", async (req, res) => {
 
 app.post("/webhook", (req, res) => {
   let body_param = req.body;
-  console.log("request", body_param);
+  // console.log("request", body_param);
 
   console.log(JSON.stringify(body_param, null, 2));
   if (body_param.object) {
     if (
       body_param.entry &&
+      Array.isArray(body_param.entry) &&
       body_param.entry[0].changes &&
-      body_param.entry[0].changes[0].value.message &&
-      body_param.entry[0].changes[0].value.message[0]
+      Array.isArray(body_param.entry[0].changes) &&
+      body_param.entry[0].changes[0].value.messages &&
+      Array.isArray(body_param.entry[0].changes[0].value.messages) &&
+      body_param.entry[0].changes[0].value.messages[0]
     ) {
       let phone_no_id =
-        body_param.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = body_param.entry[0].changes[0].value.messages[0].from;
-      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+      body_param.entry[0].changes[0].value.metadata.phone_number_id;
+    let from = body_param.entry[0].changes[0].value.messages[0].from;
+    let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;    
 
       console.log("Sending message to:", from);
       console.log("Message Body:", msg_body);
