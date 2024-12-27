9abe6d9dbf641d3023767a4547708c7d145b122b
elvisk2710 <kadeyaelvis@gmail.com>
2024-12-27 16:44:22 +0200

    tokens

--- nodejs/server.js
+++ nodejs/server.js
@@ -210,18 +210,67 @@ app.use(
 //   }
 // });
 
+const token = process.env.WHATSAPP_TOKEN
 app.get("/webhook", async (req, res) => {
   let mode = req.query["hub.mode"];
   let challenge = req.query["hub.challenge"];
   let token = req.query["hub.verify_token"];
-  const myToken = ""
-  if (mode && token){
-    if (mode === "subscribe" && token === myToken){
+  const myToken = "myToken";
+  if (mode && token) {
+    if (mode === "subscribe" && token === myToken) {
       res.status(200).send(challenge);
-    }else{
+    } else {
       res.status(403);
     }
   }
+});
+
+app.post("/webhook", (req, res) => {
+  let body_param = req.body;
+
+  console.log(JSON.stringify(body_param, null, 2));
+  if (body_param.object) {
+    if (
+      body_param.entry &&
+      body_param.entry[0].changes &&
+      body_param.entry[0].changes[0].value.message &&
+      body_param.entry[0].changes[0].value.message[0]
+    ) {
+      let phone_no_id =
+        body_param.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = body_param.entry[0].changes[0].value.messages[0].from;
+      let msg_body =
+        body_param.entry[0].changes[0].value.messages[0].text.body;
+
+      axios({
+        method: "POST",
+        url:
+          "https://graph.facebook.com/v21.0/" +
+          phone_no_id +
+          "/messages?access_token=" +
+          token,
+        headers: {
+          Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          type: "text",
+          text: {
+            body: `hello this is elvis`,
+          },
+        },
+      });
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  }
+});
+
+app.get("/",(req,res)=>{
+  res.status(200).send("lol vghvhgvgh");
 })
 // Socket.IO Configuration
 io.on("connection", (socket) => {
